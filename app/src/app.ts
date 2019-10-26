be14e33e78303fe0d2de2c3ace90305eb24f6c0d
Braydon Berthelet <bberthelet@axys.com>
2019-10-25 22:02:17 -0700

    Added headers to the image uploads Disabled err404handler for now Added navigation in each page to go home Fixed bug in the loginhandler (See issue #48)

--- app/src/app.ts
+++ app/src/app.ts
@@ -120,7 +120,7 @@ export class Server {
     // THIS MIDDLEWARE MUST COME LAST (before error handling)! todo: write tests to ensure 404 occurs w/ our handler.
     // catch not found requests, respond with 404
     // this is technically not error handling, because there was no error given, but it's how we handle 404 errors.
-    this.app.use(err404handler);
+    // this.app.use(err404handler);
 
     // error handling
     this.app.use(getErrorHandler());
