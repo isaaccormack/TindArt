8b087a17de1c0c2f6f13ded27ad37773960190f0
Isaac Cormack <i@w134-87-170-208.wireless.uvic.ca>
2019-11-05 08:37:24 -0800

    remove example test case

--- test/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { example } from '../src/example';
-
-describe('Example object', () => {
-    it('should return true', () => {
-        expect(example() == true);
-    });
-});
