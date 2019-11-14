2e222fd3b8c99e2f035a12d9c0bb0a6c2c1c70b5
Jamie St Martin <jamiestm9@hotmail.com>
2019-11-13 17:31:11 -0800

    Added synchronous database access

--- src/database/dal.ts
+++ src/database/dal.ts
@@ -1,25 +1,36 @@
-import { MongoClient, Db } from "mongodb";
+import { MongoClient, MongoError, Db } from "mongodb";
+import assert from "assert";
 
-export class Database {
-  private db: Promise<Db>;
+let db: Db;
 
-  constructor() {
-    this.db = this.connect();
+/**
+ * Initialize database connection.
+ * Calls the passed callback only once the database is connected - so
+ * this should be (and is, in bin/www) called with server.listen() as the
+ * callback. This way, the (singular) database connection is available
+ * synchonously throughout the life of the server.
+ */
+export function initDb(callback: (err: MongoError | null, db: Db | undefined) => void): void {
+  if (db) {
+    return callback(null, db);
   }
 
-  public async getUserByEmail(email: string): Promise<any> {
-
+  function onConnected(err: MongoError, client: MongoClient) {
+    if (err) {
+      return callback(err, undefined);
+    }
+    db = client.db("myapp");
+    return callback(null, db);
   }
 
-  private async connect(): Promise<Db> {
-    const client: Promise<MongoClient> = MongoClient.connect("mongodb://localhost:27017");
-    return client.then((c: MongoClient) => {
-        console.log("Connected to database");
-        return c.db("myapp");
-      })
-      .catch((err: any) => {
-        console.log("Unable to connect to database");
-        throw err;
-      });
-  }
+  MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true }, onConnected);
+}
+
+/**
+ * Access the (singular) database connection.
+ * Requires initDb() to have been called first.
+ */
+export function getDb(): Db {
+  assert.ok(db, "Db has not been initialized. Call initDb first.");
+  return db;
 }
